ad7baea804c3f34e8a034e75077ab3fb8382ae69
Pete To <peteto1213@gmail.com>
2022-12-23 21:32:11 +0800

    frontend deployment

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async(e) => {
   loader(messageDiv);
 
   // fetching data from OPENAI
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://gptdemo.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
